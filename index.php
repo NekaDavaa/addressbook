e84842debd7db16a0a16ec5a79e39b0aea6c4642
NekaDavaa <114173828+NekaDavaa@users.noreply.github.com>
2023-09-15 10:10:48 +0300

    Installing pdo class, pageloader, init file and utilities

--- index.php
+++ index.php
@@ -14,36 +14,36 @@
 			<h1>Ajax Address Book</h1>
 		</div>
 		<div class="large-6 columns">
-			<a class="add-btn button right small" data-reveal-id="myModal">Add Contact</a>
-			<div id="myModal" class="reveal-modal" data-reveal>
+			<a class="add-btn button right small" data-reveal-id="addModal">Add Contact</a>
+			<div id="addModal" class="reveal-modal" data-reveal>
 				<h2>Add Contact</h2>
-				<form>
+				<form id="addContact" action="#" method="post">
 						  <div class="row">
 							<div class="large-6 columns">
 							  <label>First Name
-								<input type="text" placeholder="Enter First Name" />
+								<input name="first_name" type="text" placeholder="Enter First Name" />
 							  </label>
 							</div>
 							 <div class="large-6 columns">
 							  <label>Last Name
-								<input type="text" placeholder="Enter Last Name" />
+								<input name="last_name" type="text" placeholder="Enter Last Name" />
 							  </label>
 							</div>
 						  </div>
 						  <div class="row">
 							<div class="large-4 columns">
 							  <label>Email
-								<input type="email" placeholder="Enter Email Address" />
+								<input name="email" type="email" placeholder="Enter Email Address" />
 							  </label>
 							</div>
 							<div class="large-4 columns">
 							  <label>Phone Number
-								<input type="text" placeholder="Enter Phone Number" />
+								<input name="phone" type="text" placeholder="Enter Phone Number" />
 							  </label>
 							</div>
 							<div class="large-4 columns">
 								<label>Contact Group
-									<select>
+									<select name="contact_group">
 										<option value="husker">Family</option>
 										<option value="starbuck">Friends</option>
 										<option value="hotdog">Business</option>
@@ -54,24 +54,24 @@
 						  <div class="row">
 							<div class="large-6 columns">
 							  <label>Address 1
-								<input type="text" placeholder="Enter Address 1" />
+								<input name="address1" type="text" placeholder="Enter Address 1" />
 							  </label>
 							</div>
 							 <div class="large-6 columns">
 							  <label>Address 2
-								<input type="text" placeholder="Enter Address 2" />
+								<input name="address2" type="text" placeholder="Enter Address 2" />
 							  </label>
 							</div>
 						  </div>
 						  <div class="row">
 							<div class="large-4 columns">
 							  <label>City
-								<input type="email" placeholder="Enter Email Address" />
+								<input name="city" type="text" placeholder="Enter City" />
 							  </label>
 							</div>
 							<div class="large-4 columns">
 								<label>State
-								<select>
+								<select name="state">
 									<option value="AL">Alabama</option>
 									<option value="AK">Alaska</option>
 									<option value="AZ">Arizona</option>
@@ -127,96 +127,34 @@
 							</div>
 							<div class="large-4 columns">
 								<label>Zipcode
-								<input type="text" placeholder="Enter Phone Number" />
+								<input name="zipcode" type="text" placeholder="Enter Phone Number" />
 							  </label>
 							</div>
 						  </div>
 						  <div class="row">
 							<div class="large-12 columns">
 							  <label>Notes
-								<textarea placeholder="Enter Optional Notes"></textarea>
+								<textarea name="notes" placeholder="Enter Optional Notes"></textarea>
 							  </label>
 							</div>
 						  </div>
-						  <input type="submit" class="add-btn button right small" value="Submit">
+						  <input name="submit" type="submit" class="add-btn button right small" value="Submit">
 							<a class="close-reveal-modal">&#215;</a>
 						</form>
 			</div>
 		</div>
 	</div>
-    <div class="row">
-		<div class="large-12 columns">
-			<table>
-				<thead>
-					<tr>
-						<th width="200">Name</th>
-						<th width="130">Phone</th>
-						<th width="200">Email</th>
-						<th width="250">Address</th>
-						<th width="100">Group</th>
-						<th width="150">Actions</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td><a href="contact.html">John Doe</a></td>
-						<td>978 388-1234</td>
-						<td>jdoe@gmail.com</td>
-						<td>
-						<ul>
-							<li>55 Main Street</li>
-							<li>Amesbury, MA 01913</li>
-						</ul>
-						</td>
-						<td>Family</td>
-						<td>
-							<ul class="button-group">
-								<li><a href="#" class="button tiny">Edit</a></li>
-								<li><a href="#" class="button tiny alert">Delete</a></li>
-							</ul>
-						</td>
-					</tr>
-					<tr>
-						<td><a href="contact.html">John Doe</a></td>
-						<td>978 388-1234</td>
-						<td>jdoe@gmail.com</td>
-						<td>
-						<ul>
-							<li>55 Main Street</li>
-							<li>Amesbury, MA 01913</li>
-						</ul>
-						</td>
-						<td>Family</td>
-						<td>
-							<ul class="button-group">
-								<li><a href="#" class="button tiny">Edit</a></li>
-								<li><a href="#" class="button tiny alert">Delete</a></li>
-							</ul>
-						</td>
-					</tr>
-					<tr>
-						<td><a href="contact.html">John Doe</a></td>
-						<td>978 388-1234</td>
-						<td>jdoe@gmail.com</td>
-						<td>
-						<ul>
-							<li>55 Main Street</li>
-							<li>Amesbury, MA 01913</li>
-						</ul>
-						</td>
-						<td>Family</td>
-						<td>
-							<ul class="button-group">
-								<li><a href="#" class="button tiny">Edit</a></li>
-								<li><a href="#" class="button tiny alert">Delete</a></li>
-							</ul>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+    
+	<!-- Loading Image -->
+	<div id="loaderImage">
+		<img src="img/ajax-loader.gif">
 	</div>
+	
+	<!-- Main Content -->
+	<div id="pageContent"></div>
+	
     <script src="js/vendor/jquery.js"></script>
+	<script src="js/script.js"></script>
     <script src="js/foundation.min.js"></script>
     <script>
       $(document).foundation();
